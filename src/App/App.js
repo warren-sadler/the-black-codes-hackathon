98308605e2d3944166a0f9109d2384f50ef1c45a
kststudio <kststudio@gmail.com>
2020-04-25 17:11:50 -0400

    starting footer build

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.scss';
 import Nav from '../components/Nav/Nav';
+import Footer from '../components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <Footer />
     </div>
   );
 }
