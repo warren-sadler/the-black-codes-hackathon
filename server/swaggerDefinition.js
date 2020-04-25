fb743cd3d585e1f8ceb4dff8cfcd26eaeb28fbf7
Warren Sadler <warrendsadler@gmail.com>
2020-04-25 11:00:42 -0500

    (chore): correct basePath

--- server/swaggerDefinition.js
+++ server/swaggerDefinition.js
@@ -2,17 +2,12 @@ const path = require("path");
 
 module.exports = {
   apis: [path.resolve(__dirname, "./modules/**/router.js")],
-  servers: [
-    {
-      url: "http://localhost:8080/api/v1",
-    },
-  ],
   swaggerDefinition: {
     info: {
       title: "The Black Codes API", // Title (required)
       version: "0.1.0", // Version (required)
       description: "The Black Codes Hackathon API", // Description (optional)
     },
-    basePath: "http://localhost:8080/api/v1", // Base path (optional)
+    basePath: "/api/v1", // Base path (optional)
   },
 };
