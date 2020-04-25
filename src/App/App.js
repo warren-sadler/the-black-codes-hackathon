643b5b139dcbd01d2d79c2cc8fb9c9b8b7efb12a
kststudio <kststudio@gmail.com>
2020-04-25 15:01:04 -0400

    files from ui into src for proper structure

---
+++ src/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.scss';
+import Nav from '../components/Nav/Nav';
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+    </div>
+  );
+}
+
+export default App;
