9a7fa8df01ebd78fc6f97fba548a330941656192
kststudio <kststudio@gmail.com>
2020-04-26 13:02:19 -0400

    hero build, removes height from navigation

--- src/App/App.js
+++ src/App/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.scss';
 
 import Nav from '../components/Nav/Nav';
+import Header from '../components/Header/Header';
 import Filter from '../components/Filter/Filter';
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <Header />
       <Filter />
     </div>
   );
